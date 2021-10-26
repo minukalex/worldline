d0f9080cd022603b7b294acd7b824fe15292126c
minukalex <minukalex@gmail.com>
2021-10-26 23:48:15 +0200

    Added scanned input Signed-off-by: minukalex <minukalex@gmail.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,35 @@
+import java.util.*;
+import java.io.*;
+
 public class Main {
     public static void main(String args[]) {
+        PrintStream output = new PrintStream(System.out);
+        Scanner input = new Scanner(System.in);
 
-        for (int i=1; i<100; i++) {
-            if (i%3==0)
-                System.out.print(" Fizz");
-            else if (i%5==0) System.out.print(" Buzz");
-            else System.out.print(" " + i);
+        Map<Integer, String> normal =  new HashMap<>();;
+        output.println("Enter your map, one key-value pair per line,");
+        output.println("comma-separated. Use empty line as sentinel.");
+        for ( String entry = input.nextLine(); entry.length() !=0;
+              entry = input.nextLine()) {
+            int comma = entry.indexOf(",");
+            Integer key = Integer.parseInt(entry.substring(0,comma));
+            String value = entry.substring(comma+1);
+            normal.put(key,value);
         }
 
+
+        for (int i=1; i<100; i++) {
+            Boolean flag = true;
+            for (Map.Entry<Integer, String> entry : normal.entrySet()) {
+                Integer key = entry.getKey();
+                String val = entry.getValue();
+                if (i % key == 0) {
+                    System.out.print(" " + val);
+                    flag = false;
+                }
+            }
+            if(flag)
+            System.out.print(" " + i);
+        }
     }
 }
